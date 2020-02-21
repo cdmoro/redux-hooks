ce136647028faebd347a0f22815258919760e98f
Carlos Bonadeo <carlosbonadeo@gmail.com>
2020-02-21 11:07:17 -0300

    user action thunk

--- src/redux/user/actions.ts
+++ src/redux/user/actions.ts
@@ -1,8 +1,30 @@
 import { AppActions } from '../types'
-import { SET_USER } from './types'
+import { SET_USER, UserActionTypes } from './types'
 import { IUser } from '../../interfaces/user.interface'
 
 export const setUserAction = (user: IUser): AppActions => ({
     type: SET_USER,
     user: user
 } as const)
+
+export const setUserActionThunk = (user: IUser): Promise<string> => {
+    return async (dispatch) => {
+        try {
+            const response = await fetch('https://rickandmortyapi.com/api/character/1')
+            const data = await response.json()
+
+            const user: IUser = {
+                name: data.name,
+                status: data.status,
+                image: data.image,
+                location: data.location.name
+            }
+
+            dispatch(setUserAction(user))
+        } catch (error) {
+            
+        }
+
+        return 'done'
+    }
+}
